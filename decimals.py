16c5ce0fa03610defbde94220e10656aa144e129
Zacharie Tournant ztournant@icloud.com <ztournant@icloud.com>
2021-11-24 23:38:34 +0300

    get info on how to round price in limit order for a specific pair

---
+++ decimals.py
@@ -0,0 +1,12 @@
+import os, math, pandas as pd
+from binance.client import Client
+
+# init
+api_key = os.environ['BINANCE_API']
+api_secret = os.environ['BINANCE_SECRET']
+client = Client(api_key, api_secret)
+
+TokenDetails = client.get_exchange_info()
+ETHTRY_filters = next(dict['filters'] for dict in TokenDetails['symbols'] if dict['symbol'] == 'ETHTRY')
+ETHTRY_price_filter = next(dict['tickSize'] for dict in ETHTRY_filters if dict['filterType'] == 'PRICE_FILTER')
+print(ETHTRY_price_filter)
